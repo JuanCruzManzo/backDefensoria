b20f86e071a7615c8f05a8f9db7ed3c451dcff6f
Juan Cruz Manzo <juancruzmanzo9@gmail.com>
2025-08-06 14:53:58 -0300

    Update principal.php

--- principal/principal.php
+++ principal/principal.php
@@ -1,4 +1,14 @@
 <?php
-include("../conexion/parametros.php");
-include("../conexion/conexion.php");
+include_once("../conexion/parametros.php");
+include_once("../conexion/conexion.php");
+include_once('../plantilla/head.php');
 ?>
+
+<!-- CONTENIDO PRINCIPAL -->
+<main class="container mt-4">
+  <h1>Bienvenido a la Defensoría del Pueblo</h1>
+</main>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
