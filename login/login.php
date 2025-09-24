05f1e5c4aed6418bf45d462688a27fca84f3eaa8
Juan Cruz Manzo <juancruzmanzo9@gmail.com>
2025-09-24 14:50:11 -0300

    empece a declarar los parametros

--- login/login.php
+++ login/login.php
@@ -1,10 +1,10 @@
 <?php
-include("../conexion/parametros.php");
-include("../conexion/conexion.php");
+include_once $_SERVER['DOCUMENT_ROOT'] . '/DefensoriaBack/backDefensoria/parametros.php';
+include(CONEXION);
+
 ?>
 <!doctype html>
 <html lang="es">
-
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
