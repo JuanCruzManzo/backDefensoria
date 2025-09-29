a09009ec121a443b5ef14e4ab160afffb76098c3
Juan Cruz Manzo <juancruzmanzo9@gmail.com>
2025-09-29 11:46:02 -0300

    s

--- conexion/parametros.php
+++
@@ -1,6 +0,0 @@
-<?php
-if (!defined("HOST")) define("HOST", "localhost");
-if (!defined("USUARIO")) define("USUARIO", "root");
-if (!defined("PASS")) define("PASS", "");
-if (!defined("DB")) define("DB", "defensoria");
-?>
